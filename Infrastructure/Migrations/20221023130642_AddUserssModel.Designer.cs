79a95d649d166202b9099e26775df3bcde0c2c47
Celilhan <celilhan12363@gmail.com>
2022-10-23 16:47:19 +0300

    Created login&register pages

--- Infrastructure/Migrations/20221023130642_AddUserssModel.Designer.cs
+++ Infrastructure/Migrations/20221023130642_AddUserssModel.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ECommerceDbContext))]
-    partial class ECommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221023130642_AddUserssModel")]
+    partial class AddUserssModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -142,7 +144,7 @@ namespace Infrastructure.Migrations
                         .HasColumnType("varchar(100)");
 
                     b.Property<string>("Password")
-                        .HasColumnType("varchar(255)");
+                        .HasColumnType("varchar(33)");
 
                     b.Property<byte>("Status")
                         .ValueGeneratedOnAdd()
--- Web/Controllers/LoginController.cs
+++ Web/Controllers/LoginController.cs
@@ -33,15 +33,18 @@ namespace Web.Controllers
             bool isAuthenticated = false;
             try
             {
-                user = userService.GetUser(userLoginDto.Email, AppUtilities.EncryptSHA256(userLoginDto.Password)); 
-                if (user != null)
+                if (ModelState.IsValid)
                 {
-                    isAuthenticated = true;
-                    HttpContext.Session.SetObjectAsJson("User", user);
-                }
-                else
-                {
-                    TempData["AlertType"] = SweetAlertTypes.UserEmailPassWordInCorrect.ToString();
+                    user = userService.GetUser(userLoginDto.Email, AppUtilities.EncryptSHA256(userLoginDto.Password));
+                    if (user != null)
+                    {
+                        isAuthenticated = true;
+                        HttpContext.Session.SetObjectAsJson("User", user);
+                    }
+                    else
+                    {
+                        TempData["AlertType"] = SweetAlertTypes.UserEmailPassWordInCorrect.ToString();
+                    }
                 }
             }
             catch (Exception ex)
@@ -53,6 +56,91 @@ namespace Web.Controllers
             if (isAuthenticated)
             {
                 return RedirectToAction("ListFaqs", "Faq");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(UserLoginDto userLoginDto)
+        {
+            User user = null;
+            bool isAuthenticated = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    user = userService.GetUser(userLoginDto.Email, AppUtilities.EncryptSHA256(userLoginDto.Password));
+                    if (user != null)
+                    {
+                        isAuthenticated = true;
+                        HttpContext.Session.SetObjectAsJson("User", user);
+                        TempData["AlertType"] = SweetAlertTypes.Login.ToString();
+                    }
+                    else
+                    {
+                        TempData["AlertType"] = SweetAlertTypes.UserEmailPassWordInCorrect.ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["AlertType"] = SweetAlertTypes.Error.ToString();
+                Console.WriteLine(ex);
+            }
+
+            if (isAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(UserRegisterDto userRegisterDto)
+        {
+            User user = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    userRegisterDto.Password = AppUtilities.EncryptSHA256(userRegisterDto.Password);
+                    user = mapper.Map<User>(userRegisterDto);
+                    user.UserType = UserTypes.User;
+                    userService.AddUser(user);
+                    TempData["AlertType"] = SweetAlertTypes.Register.ToString();
+                }
+                else
+                {
+                    TempData["AlertType"] = SweetAlertTypes.Error.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["AlertType"] = SweetAlertTypes.Error.ToString();
+                Console.WriteLine(ex);
+            }
+
+            if (user.Id > 0)
+            {
+                return RedirectToAction("Login", "Login");
             }
             else
             {
