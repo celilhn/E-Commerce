2070e7355bd6e02438f328491537e5b568b1709c
Celilhan <celilhan12363@gmail.com>
2022-10-23 13:29:53 +0300

    Added user model & service & repository

---
+++ Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using static Domain.Constants.Constants;
+
+namespace Domain.Models
+{
+    public class User : ExtendedBaseModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ImageUrl { get; set; }
+        public UserTypes UserType { get; set; }
+    }
+}
---
+++ Infrastructure/Configurations/UserConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations
+{
+    public class UserConfigurations : ExtendedBaseModelConfiguration<User>
+    {
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            base.Configure(builder);
+            builder.Property(x => x.Name).HasColumnType("varchar(100)");
+            builder.Property(x => x.Surname).HasColumnType("varchar(100)");
+            builder.Property(x => x.UserName).HasColumnType("varchar(780)");
+            builder.Property(x => x.Email).HasColumnType("varchar(100)");
+            builder.Property(x => x.Password).HasColumnType("varchar(33)");
+            builder.Property(x => x.ImageUrl).HasColumnType("varchar(256)");
+            builder.Property(x => x.UserType).HasColumnType("int");
+        }
+    }
+}
