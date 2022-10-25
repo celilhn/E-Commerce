b3a82f286dce774cc69dff6cc77f1c4bf3fd7d1b
Celilhan <celilhan12363@gmail.com>
2022-10-25 21:20:29 +0300

    Created slider model & service & repository

---
+++ Application/Interfaces/ISliderService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Domain.Models;
+using static Domain.Constants.Constants;
+
+namespace Application.Interfaces
+{
+    public interface ISliderService
+    {
+        Slider AddSlider(Slider slider);
+        Slider UpdateSlider(Slider slider);
+        Slider GetSlider(int id);
+        List<Slider> GetSliders();
+        List<Slider> GetSliders(StatusCodes statusCodes);
+    }
+}
---
+++ Domain/Interfaces/ISliderRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Domain.Models;
+using static Domain.Constants.Constants;
+
+namespace Domain.Interfaces
+{
+    public interface ISliderRepository
+    {
+        Slider AddSlider(Slider slider);
+        Slider UpdateSlider(Slider slider);
+        Slider GetSlider(int id);
+        List<Slider> GetSliders();
+        List<Slider> GetSliders(StatusCodes statusCodes);
+    }
+}
