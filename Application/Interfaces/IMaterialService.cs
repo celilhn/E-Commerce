f50ebbbd8df94d29f2a718c8cd4e88049f2bf638
Celilhan <celilhan12363@gmail.com>
2022-11-27 22:09:01 +0300

    created material model&services&repositories

---
+++ Application/Interfaces/IMaterialService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Domain.Models;
+using static Domain.Constants.Constants;
+
+namespace Application.Interfaces
+{
+    public interface IMaterialService
+    {
+        Material AddMaterial(Material material);
+        Material GetMaterial(int Id);
+        Material UpdateMaterial(Material material);
+        List<Material> GetMaterials();
+        List<Material> GetMaterials(StatusCodes status);
+        bool IsMaterialExist(string name);
+        bool ControlMaterialIsExistWithParameters(int id, string name);
+    }
+}
